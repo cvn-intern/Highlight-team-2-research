f046402e49053a80a07e627de0e720cea74db154
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-06-20 14:02:59 +0700

    orm

--- sql_injection/app.js
+++ sql_injection/app.js
@@ -1,20 +1,43 @@
-// {
-    const express = require('express');
-    const bodyParser = require('body-parser');
-    const serveStatic = require('serve-static');
-    const sqlite3 = require('sqlite3').verbose();
-    
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const serveStatic = require('serve-static');
+const sqlite3 = require('sqlite3').verbose();
+const { Sequelize, DataTypes } = require("sequelize")
+
+const main = async () => {
+
     const app = express();
     app.use(serveStatic(__dirname+ '/public', {'index': ['index.html']}));
     app.use(bodyParser.urlencoded({extended: true}));
     
-    const db = new sqlite3.Database(':memory:');
-    db.serialize(function() {
-      db.run("CREATE TABLE user (username TEXT, password TEXT, name TEXT)");
-      db.run("INSERT INTO user VALUES ('admin', 'admin123', 'App Administrator'), ('admin2', 'admin123', 'App Administrator 2')");
+    // const db = new sqlite3.Database(':memory:');
+    // db.serialize(function() {
+    //   db.run("CREATE TABLE user (username TEXT, password TEXT, name TEXT)");
+    //   db.run("INSERT INTO user VALUES ('admin', 'admin123', 'App Administrator')");
+    // });
+
+    //orm config
+    const sequelize = new Sequelize('sqlite::memory:', {
+        define: {
+          freezeTableName: true
+        }
+      });
+    const User = sequelize.define('user', {
+        username: DataTypes.TEXT,
+        password: DataTypes.TEXT,
+        name: DataTypes.TEXT,
     });
-    // }
-    app.post('/login', function (req, res) {
+
+    await User.sync()
+
+    await User.create({
+        username: 'admin',
+        password: 'admin123',
+        name: 'App Administrator',
+    })
+   
+    app.post('/login', async function (req, res) {
         const username = req.body.username; // a valid username is admin
         const password = req.body.password; // a valid password is admin123 unknown' or '1'='1
         const query = "SELECT name FROM user where username = '" + username + "' and password = '" + password + "'";
@@ -23,38 +46,42 @@
         console.log("password: " + password);
         console.log('query: ' + query);
         
-        // db.get(query , function(err, row) {
-        //     console.log({row})
+        // db.get(query , function(err, value) {
+        //     console.log({value})
             
         //     if(err) {
         //         console.log('ERROR', err);
         //         res.redirect("/index.html#error");
-        //     } else if (!row) {
+        //     } else if (!value) {
         //         res.redirect("/index.html#unauthorized");
         //     } else {
-        //         res.send({row});
+        //         res.send({value});
         //     }
         // });
 
         /* parameters */
-        const queryPrepare = db.prepare("SELECT name FROM user where username = (?) and password = (?)");
-        queryPrepare.each(username, password, function(err, row) {
-            res.send({row})
-        }, function(err, count) {
-            res.send({msg: "Invalid credentials"})
-            queryPrepare.finalize();
-        });
-      
-    
-    });
+        // const queryPrepare = db.prepare("SELECT name FROM user where username = (?) and password = (?)");
+        // queryPrepare.each(username, password, function(err, value) {
+        //     res.send({value})
+        // }, function(err, count) {
+        //     res.send({msg: "Invalid credentials"})
+        //     queryPrepare.finalize();
+        // });
+
+        /* orm */
+        User.findOne({where: {
+            username,
+            password
+        }, attributes: ['name']}).then(value => res.send({value})).catch(err => res.send({msg: "Invalid credentials"})) 
 
     
-   
-    // orm
+    });
 
     // sanitizing
     
 
 app.listen(3000, function() { console.log('listening'); });
     
-    
+}
+
+main()
