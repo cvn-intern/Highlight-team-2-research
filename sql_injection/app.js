e47f555f5813e5c2e49fcb71e8f8c52d56956674
khoatruong19 <thpthv.truonghuynhdangkhoa@gmail.com>
2023-06-20 13:25:46 +0700

    parameters

--- sql_injection/app.js
+++ sql_injection/app.js
@@ -1,42 +1,60 @@
 // {
-    var express = require('express');
-    var bodyParser = require('body-parser');
+    const express = require('express');
+    const bodyParser = require('body-parser');
     const serveStatic = require('serve-static');
-    var sqlite3 = require('sqlite3').verbose();
+    const sqlite3 = require('sqlite3').verbose();
     
-    var app = express();
+    const app = express();
     app.use(serveStatic(__dirname+ '/public', {'index': ['index.html']}));
     app.use(bodyParser.urlencoded({extended: true}));
     
-    var db = new sqlite3.Database(':memory:');
+    const db = new sqlite3.Database(':memory:');
     db.serialize(function() {
       db.run("CREATE TABLE user (username TEXT, password TEXT, name TEXT)");
-      db.run("INSERT INTO user VALUES ('admin', 'admin123', 'App Administrator')");
+      db.run("INSERT INTO user VALUES ('admin', 'admin123', 'App Administrator'), ('admin2', 'admin123', 'App Administrator 2')");
     });
     // }
     app.post('/login', function (req, res) {
-        var username = req.body.username; // a valid username is admin
-        var password = req.body.password; // a valid password is admin123 unknown' or '1'='1
-        var query = "SELECT name FROM user where username = '" + username + "' and password = '" + password + "'";
+        const username = req.body.username; // a valid username is admin
+        const password = req.body.password; // a valid password is admin123 unknown' or '1'='1
+        const query = "SELECT name FROM user where username = '" + username + "' and password = '" + password + "'";
     
         console.log("username: " + username);
         console.log("password: " + password);
         console.log('query: ' + query);
         
-        db.get(query , function(err, row) {
-    
-            if(err) {
-                console.log('ERROR', err);
-                res.redirect("/index.html#error");
-            } else if (!row) {
-                res.redirect("/index.html#unauthorized");
-            } else {
-                res.send('Hello <b>' + row.name + '</b><br /><a href="/index.html">Go back to login</a>');
-            }
+        // db.get(query , function(err, row) {
+        //     console.log({row})
+            
+        //     if(err) {
+        //         console.log('ERROR', err);
+        //         res.redirect("/index.html#error");
+        //     } else if (!row) {
+        //         res.redirect("/index.html#unauthorized");
+        //     } else {
+        //         res.send({row});
+        //     }
+        // });
+
+        /* parameters */
+        const queryPrepare = db.prepare("SELECT name FROM user where username = (?) and password = (?)");
+        queryPrepare.each(username, password, function(err, row) {
+            res.send({row})
+        }, function(err, count) {
+            res.send({msg: "Invalid credentials"})
+            queryPrepare.finalize();
         });
+      
     
     });
+
     
-    app.listen(3000, function() { console.log('listening'); });
+   
+    // orm
+
+    // sanitizing
+    
+
+app.listen(3000, function() { console.log('listening'); });
     
     
